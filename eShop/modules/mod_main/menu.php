4d0fa3501a42db4042d334dd87b98af060679fcb
setcode <setcode>
2004-08-16 20:36:24 +0000

    *** empty log message ***

--- eShop/modules/mod_main/menu.php
+++ eShop/modules/mod_main/menu.php
@@ -6,10 +6,27 @@
 * @ Authors : 2004 T. Prêtre & R. Emourgeon
 * @ eShop is Free Software
 * @ Released under GNU/GPL License : http://www.gnu.org/copyleft/gpl.html
-* $Id: menu.php,v 1.3 2004/07/10 20:32:06 kilgore Exp $
+* $Id: menu.php,v 1.4 2004/08/16 20:36:24 setcode Exp $
 **/
 
 defined( '_DIRECT_ACCESS' ) or die(header("Location: ../../erreur.html"));
+
+//assign active modules
+$query = "SELECT * FROM ".$GLOBALS["db_prefix"]."_active_mod am WHERE am.am_status='1'";
+if(!$resultat = &$connexion->Execute($query))
+echo $connexion->ErrorMsg();
+$active_mod = $resultat->GetArray();
+$count = count($active_mod);
+//print_r($active_mod);
+//echo "<br >". $count;
+if($count > 0)
+{
+	for($i = 0; $i < $count; ++$i)
+	{
+		$template->assign('mod_'.$active_mod[$i][am_name], true);
+	}
+}
+
 
 $query = "SELECT ca_id, ca_name FROM ".$db_prefix."_cat WHERE ca_cat_FK=0";
 
